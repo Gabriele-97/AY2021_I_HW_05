15cea2114e36208d4031d96a0889ad5aabe83202
Gabriele Fassina <gfassina.97@gmail.com>
2020-11-06 15:43:46 +0100

    startup register set

--- FASSINA_GABRIELE.cydsn/main.c
+++ FASSINA_GABRIELE.cydsn/main.c
@@ -11,10 +11,15 @@
 */
 #include "project.h"
 
+#define STARTUP 0x0000
+
 int main(void)
 {
+    uint8 sampling_freq;
+    
     CyGlobalIntEnable; /* Enable global interrupts. */
-
+    EEPROM_Start();
+    sampling_freq = EEPROM_ReadByte(STARTUP);
     /* Place your initialization/startup code here (e.g. MyInst_Start()) */
 
     for(;;)
